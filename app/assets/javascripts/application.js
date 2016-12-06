5d172727f42ca68c7a068566a6b740b6956c0d60
aleks2414 <alejandroromo14@gmail.com>
2016-12-06 13:40:40 -0600

    Mini browser

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,22 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
-//= require jquery
 //= require bootstrap-sprockets
+
+$(document).ready(function () {
+    $(".test").click(function () {
+        $("#thedialog").attr('src', $(this).attr("href"));
+        $("#somediv").dialog({
+            width: 400,
+            height: 450,
+            modal: true,
+            close: function () {
+                $("#thedialog").attr('src', "about:blank");
+            }
+        });
+        return false;
+    });
+});
