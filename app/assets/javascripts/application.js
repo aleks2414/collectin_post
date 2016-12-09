305b80617316b44a5057cdd1fa1d046e9e6713bc
aleks2414 <alejandroromo14@gmail.com>
2016-12-08 21:58:46 -0600

    Zack's corrections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,18 +15,3 @@
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap-sprockets
-
-$(document).ready(function () {
-    $(".test").click(function () {
-        $("#thedialog").attr('src', $(this).attr("href"));
-        $("#somediv").dialog({
-            width: 400,
-            height: 450,
-            modal: true,
-            close: function () {
-                $("#thedialog").attr('src', "about:blank");
-            }
-        });
-        return false;
-    });
-});
